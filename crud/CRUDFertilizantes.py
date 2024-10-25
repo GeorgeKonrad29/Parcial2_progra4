ffd7d9ae88f90a8d4d67f0b19b816a528bcec128
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2024-10-25 16:15:36 -0500

    Crud

---
+++ crud/CRUDFertilizantes.py
@@ -0,0 +1,8 @@
+#necesito crear clases estructurales de todas las clases que tengo en el modelo
+#para poder hacer el crud
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ 
+
