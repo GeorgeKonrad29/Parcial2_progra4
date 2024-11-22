f2f52b49fa5607d597760305bccf586e00a1ca81
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2024-11-21 22:41:46 -0500

    informacionClienteEntero

---
+++ Controller/controlerMostrarCliente.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from model.clientes import Clientes
+from crud.CRUDClientes import CrudClientes
+
+class controlerMostrarCliente():
+    def buscarPorCedula(self,clientes,cedula):
+        cliente = CrudClientes.buscar_por_cedula(clientes,cedula)
+        return cliente
